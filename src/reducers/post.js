aa629cde6a3772fd55cd213c66f083a3cb421f21
Anthony DiRusso <ossurid@gmail.com>
2019-05-09 09:58:55 -0400

    adds store that uses post reducer

--- src/reducers/post.js
+++ src/reducers/post.js
@@ -4,7 +4,7 @@ import { ACTION_CREATE_POST } from "../actions";
 // the current state and an action 
 // then caluculates and returns the next new 
 // version of state
-export function posts(state={}, action={ type: '' }) {
+export default function posts(state={}, action={ type: '' }) {
     switch(action.type) {
         case ACTION_CREATE_POST:
             console.log(`Sounds like you want to create a post`);
